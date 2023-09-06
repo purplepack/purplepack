f80879aaa95f9c59919b066072ef0408d217b604
Rexedge <ap.oyeniran@gmail.com>
2023-09-06 20:18:09 +0100

    Form

--- src/emma/about_su/enquiries.tsx
+++ src/emma/about_su/enquiries.tsx
@@ -1,38 +1,82 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+'use client';
+import { Button } from '@/components/ui/button';
+import React from 'react';
 
 export default function Enquiries() {
-  return (
-    <div>
-      <div className=" flex flex-col justify-center p-10 w-full gap-3">
-        <input
-          className=" w-full h-12 outline focus:outline bg-inherit pl-5"
-          type="text"
-          placeholder="Full name"
-        />
-        <input
-          className=" w-full h-12 outline focus:outline bg-inherit pl-5"
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          className=" w-full h-12 outline focus:outline bg-inherit pl-5"
-          type="tel"
-          placeholder="Phone"
-        />
-        <input
-          className=" w-full h-12 outline focus:outline bg-inherit pl-5"
-          type="text"
-          placeholder="Nature of enquiry"
-        />
-        <input
-          className=" w-full h-28 pb-16 outline focus:outline bg-inherit pl-5"
-          type="text"
-          placeholder="Message"
-        />
-        <Button className=" bg-black" type='submit'>SUBMIT</Button>
-      </div>
-      
-    </div>
-  );
+	const [formData, setFormData] = React.useState({
+		name: '',
+		email: '',
+		phone: '',
+		subject: '',
+		body: '',
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const generateWhatsAppLink = () => {
+		const { name, email, phone, subject, body } = formData;
+
+		// Build the WhatsApp link
+		const whatsappText = `Name: ${name}%0AEmail: ${email}%0APhone: ${phone}%0ASubject: ${subject}%0ABody: ${body}`;
+		const whatsappLink = `https://wa.me/2348061719533/?text=${whatsappText}`;
+
+		// Redirect the user to WhatsApp
+		window.location.href = whatsappLink;
+	};
+
+	return (
+		<div>
+			<form className=' flex flex-col justify-center p-10 w-full gap-3'>
+				<input
+					className=' w-full h-12 outline focus:outline bg-inherit pl-5'
+					type='text'
+					name='name'
+					placeholder='Full name'
+					required
+					onChange={handleChange}
+				/>
+				<input
+					className=' w-full h-12 outline focus:outline bg-inherit pl-5'
+					type='email'
+					name='email'
+					required
+					placeholder='Email'
+					onChange={handleChange}
+				/>
+				<input
+					className=' w-full h-12 outline focus:outline bg-inherit pl-5'
+					type='tel'
+					name='phone'
+					required
+					placeholder='Phone'
+					onChange={handleChange}
+				/>
+				<input
+					className=' w-full h-12 outline focus:outline bg-inherit pl-5'
+					type='text'
+					name='subject'
+					required
+					placeholder='Nature of enquiry'
+					onChange={handleChange}
+				/>
+				<input
+					className=' w-full h-28 pb-16 outline focus:outline bg-inherit pl-5'
+					type='text'
+					name='body'
+					placeholder='Message'
+					onChange={handleChange}
+					required
+				/>
+				<Button
+					className=' bg-black'
+					onClick={generateWhatsAppLink}
+				>
+					SUBMIT
+				</Button>
+			</form>
+		</div>
+	);
 }
