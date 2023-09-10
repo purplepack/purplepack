8b1b13d3081747e369437feaff18799fd5e03f5b
Rexedge <ap.oyeniran@gmail.com>
2023-09-10 09:20:03 +0100

    blog fix

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@/components/layout/container';
 import { blogPosts } from '@/lib/data';
 import Image from 'next/image';
 import React from 'react';
@@ -10,19 +11,21 @@ export default function IndividualPost({ params }: { params: { id: string } }) {
 		return <div>Blog post not found.</div>;
 	}
 	return (
-		<div>
-			<h1 className='text-3xl font-semibold my-4'>{post.title}</h1>
-			<p className='text-gray-500 text-sm mb-2'>
-				{post.date} by {post.author}
-			</p>
-			<Image
-				width={350}
-				height={400}
-				src={post.image}
-				alt={post.title}
-				className='rounded-lg mb-4'
-			/>
-			<div className='text-gray-700'>{post.content}</div>
-		</div>
+		<Container>
+			<div className='py-40'>
+				<h1 className='text-3xl font-semibold '>{post.title}</h1>
+				<p className='text-gray-500 text-sm mb-2'>
+					{post.date} by {post.author}
+				</p>
+				<Image
+					width={350}
+					height={400}
+					src={post.image}
+					alt={post.title}
+					className='rounded-lg mb-4'
+				/>
+				<div className='text-gray-700'>{post.content}</div>
+			</div>
+		</Container>
 	);
 }
