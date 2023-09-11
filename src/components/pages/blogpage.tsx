e70076d6d433c52ba941a017067edd90e64006e2
Rexedge <ap.oyeniran@gmail.com>
2023-09-11 16:18:43 +0100

    update

--- src/components/pages/blogpage.tsx
+++ src/components/pages/blogpage.tsx
@@ -5,11 +5,12 @@ export default function BlogPage({ posts }: { posts: PostI[] }) {
 	return (
 		<div className='py-10 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-10 '>
 			{posts.map((post, i) => (
-				<BlogCard
-					key={i}
-					post={post}
-					i={i}
-				/>
+				// <BlogCard
+				// 	key={i}
+				// 	post={post}
+				// 	i={i}
+				// />
+				<>{post.title}</>
 			))}
 		</div>
 	);
